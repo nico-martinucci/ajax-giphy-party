5f90b2b2afd00518c0e6bcf1a6673318405baecc
Nico Martinucci <domenico.martinucci@gmail.com>
2022-11-15 15:46:44 -0800

    add function structure

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Let's get this party started!");
+"use strict";
+
+// function to get input data
+function getFormInput() {
+    return $("#search-term").val();
+}
+
+// async function to request gif from giphy
+async function getGIF() {
+
+}
+
+// function to update dom with new gif
+function addGIF() {
+
+}
+
+// controller function for the search button
+function getInputAndUpdatePage() {
+
+}
+
+// function to clear the gifs div
+function removeGIFs() {
+    $("#gifs").empty();
+}
+
+
+// listener for search button
+$("#search").on("click", getInputAndUpdatePage);
+
+// listener for remove button
+$("#remove-all").on("click", removeGIFs);
