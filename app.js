223ec8e809ca185b64ed6da7eb88f2a52d2e5b9c
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2022-11-15 16:21:51 -0800

    did a bunch. almost there

--- app.js
+++ app.js
@@ -6,8 +6,18 @@ function getFormInput() {
 }
 
 // async function to request gif from giphy
-async function getGIF() {
+async function getGIF(input) {
+    const randomOffset = Math.round(Math.random() * 4999);
 
+    let gifPull = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            api_key: "L1vsHUAqEURXHTDiIQcxZALO77jEe9E7",
+            q: input,
+            limit: 1,
+            offset: randomOffset
+        }
+    })
+    console.log("the gif stuff is ", gifPull)
 }
 
 // function to update dom with new gif
@@ -16,8 +26,10 @@ function addGIF() {
 }
 
 // controller function for the search button
-function getInputAndUpdatePage() {
-
+function getInputAndUpdatePage(evt) {
+    evt.preventDefault();
+    let formInput = getFormInput();
+    getGIF(formInput);
 }
 
 // function to clear the gifs div
@@ -31,3 +43,7 @@ $("#search").on("click", getInputAndUpdatePage);
 
 // listener for remove button
 $("#remove-all").on("click", removeGIFs);
+
+
+//api key:
+// L1vsHUAqEURXHTDiIQcxZALO77jEe9E7
